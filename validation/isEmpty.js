397f480190279a829d634660f9080caf0578b36c
expreun9 <expreun9@gmail.com>
2018-08-10 12:43:40 -0500

    Created global isEmpty method to add support for data types other than string, when using validator. Did this instead of using another package like _lodash because it is simple and pointless to include a whole library for one function.

---
+++ validation/isEmpty.js
@@ -0,0 +1,9 @@
+// Could use _lodash but why use a whole library when you can create it
+
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === 'object' && Object.keys(value).length === 0) ||
+  (typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;
