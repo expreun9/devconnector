6c61eb409e45e8d2746bff79092789e502d5d14a
expreun9 <expreun9@gmail.com>
2018-08-10 08:59:41 -0500

    Fixed typo in User Model at the end of the file. Should of been "UserSchema" not "User"

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Skema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 // Create Schema
 const UserSchema = new Schema({
@@ -24,4 +24,4 @@ const UserSchema = new Schema({
   }
 });
 
-module.exports = User = mongoose.model('users', User);
+module.exports = User = mongoose.model('users', UserSchema);
