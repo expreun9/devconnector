4a4edd123797d16894359ea8ebb853369be8f084
expreun9 <expreun9@gmail.com>
2018-09-29 19:25:32 -0500

    Frontend added
    Using material ui / material kit react

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from "./components/Landing";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <p>App</p>
+          <Route exact path="/" component={Landing} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/routes/index.js
@@ -0,0 +1,11 @@
+import LandingPage from "../views/LandingPage/LandingPage.js";
+//import ProfilePage from "../views/ProfilePage/ProfilePage.js";
+//import LoginPage from "../views/LoginPage/LoginPage.js";
+
+var indexRoutes = [
+  { path: "/landing-page", name: "LandingPage", component: LandingPage }
+  //{ path: "/profile-page", name: "ProfilePage", component: ProfilePage },
+  //{ path: "/login-page", name: "LoginPage", component: LoginPage }
+];
+
+export default indexRoutes;
